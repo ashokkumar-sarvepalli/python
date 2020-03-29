467bb0f55b979ebfefb0e798a017475b755dd580
ASHOKKUMAR SARVEPALLI <ashok@ASHOKKUMARs-MacBook-Pro.local>
2020-03-29 14:03:04 -0400

    new program

---
+++ njcovidcases-percentage/njcoviddeaths.py
@@ -0,0 +1,39 @@
+def readFile(path):
+    with open(path, "rt") as f:
+        return f.read()
+
+def buildPercentageChangeTable(contentsRead):
+    i=0
+    
+    previousDay = 0
+    currentDay= 0
+    percentageChange = {}
+    # its read as one massive string
+    print(contentsRead)
+    for line in contentsRead.splitlines():
+        columns = line.split(",")
+        if i>1:
+            arr = []
+            currentDay= int(columns[2])
+            currentPercentage = ((currentDay-previousDay)/previousDay)*100
+            currentDate = columns[0]
+            arr.append(currentDay)
+            arr.append(currentPercentage)
+            percentageChange[currentDate] = arr
+            #print("The percentage increase from ",previousDate,"to ",currentDate, "is : ", currentPercentage, "%")
+            previousDay = currentDay
+            previousDate = currentDate
+        if i==1:
+            previousDay= int(columns[2])
+            previousDate =columns[0]
+        i=i+1
+
+    return percentageChange
+
+
+contentsRead = readFile("njcoviddeaths.csv")
+percentageChange = buildPercentageChangeTable(contentsRead)
+print(percentageChange)
+for key in percentageChange:
+    print(key,":",percentageChange[key])
+
